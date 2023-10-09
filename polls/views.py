dd1ae95c6f4d9cd6f7932df4ae333c9742f1a2fd
oyamadayus <117361496+oyamadayus@users.noreply.github.com>
2023-10-09 16:28:14 +0900

    timezone.now()とdatetime.now()

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from django.utils import timezone
 
 
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index. Now:{datetime.datetime.now()}")
+    return HttpResponse(f"Hello, world. You're at the polls index. datetime.now:{datetime.datetime.now()}, {timezone.now()}")
 # Create your views here.
