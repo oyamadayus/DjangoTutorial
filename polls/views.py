a440159cedbd81b7477cae3b2e1f9acc47e9b3c2
oyamadayus <117361496+oyamadayus@users.noreply.github.com>
2023-10-09 17:05:08 +0900

    3-もっとビューを書いてみる

--- polls/views.py
+++ polls/views.py
@@ -6,4 +6,17 @@ from django.utils import timezone
 
 def index(request):
     return HttpResponse(f"Hello, world. You're at the polls index. datetime.now:{datetime.datetime.now()}, {timezone.now()}")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 # Create your views here.
